6c0a5a4bf0a95a3ed408e2ca5e5f2edc348c0864
Nurlan Nugumanov <r2d2@yandex-team.ru>
2022-10-09 13:29:25 +0600

    5 sprint, E_search_tree.py

--- sprint_5/E_search_tree.py
+++ sprint_5/E_search_tree.py
@@ -1,5 +1,8 @@
 # ! change LOCAL to False before submitting !
 # set LOCAL to True for local testing
+from __future__ import annotations
+
+from typing import Union
 
 LOCAL = False
 
@@ -11,38 +14,54 @@ if LOCAL:
             self.left = left
 
 
+def get_values(root, course: str) -> (bool, Union[int|None]):
+
+    if root is None:
+        return True, None
+
+    is_rbst, rvalue = get_values(root.right, "left")
+    is_lbst, lvalue = get_values(root.left, "right")
+
+    if not is_lbst or not is_rbst:
+        return False, None
+
+    if lvalue is not None:
+        if not (lvalue < root.value):
+            return False, None
+
+    if rvalue is not None:
+        if not (rvalue > root.value):
+            return False, None
+
+    if course == "right":
+        if rvalue is None:
+            rvalue = root.value
+        return True, rvalue
+
+    if course == "left":
+        if lvalue is None:
+            lvalue = root.value
+        return True, lvalue
+
+    return True, None
+
+
 def solution(root) -> bool:
     #  Your code
     #  “ヽ(´▽｀)ノ”
+    return get_values(root, '')[0]
 
-    if root.right is not None:
-        if root.value >= root.right.value:
-            return False
-
-        is_right_part_bst = solution(root.right)
-        if not is_right_part_bst:
-            return is_right_part_bst
-
-    if root.left is not None:
-        if root.value <= root.left.value:
-            return False
-
-        is_left_part_bst = solution(root.left)
-        if not is_left_part_bst:
-            return False
-
-    return True
 
 def test():
-    # node1 = Node(1, None, None)
-    # node2 = Node(4, None, None)
-    # node3 = Node(3, node1, node2)
-    # node4 = Node(8, None, None)
-    # node5 = Node(5, node3, node4)
-    #
-    # assert solution(node5)
-    # node2.value = 5
-    # assert not solution(node5)
+    node1 = Node(1, None, None)
+    node2 = Node(4, None, None)
+    node3 = Node(3, node1, node2)
+    node4 = Node(8, None, None)
+    node5 = Node(5, node3, node4)
+
+    assert solution(node5)
+    node2.value = 5
+    assert not solution(node5)
 
     _node1 = Node(5, None, None)
     _node2 = Node(3, None, None)
@@ -58,6 +77,13 @@ def test():
     n1 = Node(3, n3, n4)
     n0 = Node(5, n1, n2)
     assert not solution(n0)
+
+    n4 = Node(7, None, None)
+    n3 = Node(1, None, None)
+    n2 = Node(8, None, None)
+    n1 = Node(3, n3, n4)
+    n01 = Node(5, n1, n2)
+    assert not solution(n01)
 
 
 if __name__ == '__main__':
