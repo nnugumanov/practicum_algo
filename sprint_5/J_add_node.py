f19fdf6a4556019d2211bb08dfbaf262567aaefe
Nurlan Nugumanov <r2d2@yandex-team.ru>
2022-10-10 23:23:48 +0600

    5 sprint, J_add_node.py

---
+++ sprint_5/J_add_node.py
@@ -0,0 +1,43 @@
+# ! change LOCAL to False before submitting !
+# set LOCAL to True for local testing
+
+LOCAL = True
+
+if not LOCAL:
+    from node import Node
+
+if LOCAL:
+    class Node:
+        def __init__(self, left=None, right=None, value=0):
+            self.right = right
+            self.left = left
+            self.value = value
+
+
+def insert(root, key):
+    #  Your code
+    #  “ヽ(´▽｀)ノ”
+    if root.value <= key:
+        if root.right is None:
+            root.right = Node(value=key)
+        else:
+            insert(root.right, key)
+    else:
+        if root.left is None:
+            root.left = Node(value=key)
+        else:
+            insert(root.left, key)
+    return root
+
+
+def test():
+    node1 = Node(None, None, 7)
+    node2 = Node(node1, None, 8)
+    node3 = Node(None, node2, 7)
+    new_head = insert(node3, 6)
+    assert new_head is node3
+    assert new_head.left.value == 6
+
+
+if __name__ == '__main__':
+    test()
